d8afa2092b5eaf14ee540d9ff5a794c58747bd29
carletex <oceanrdn@gmail.com>
2014-07-24 12:57:07 -0400

    Using Buffers instead of strings. Bdecoder working

--- bencoding.js
+++ bencoding.js
@@ -1,10 +1,20 @@
+'use strict';
+
 // Required modules
 var fs = require('fs');
+var buffer = require('buffer');
+
+// Contants
+var i_CHAR = 105;
+var l_CHAR = 108;
+var d_CHAR = 100;
+var e_CHAR = 101;
+var colon_CHAR = 58;
 
 var filename = process.argv[2];
 
 try {
-	var file_content = fs.readFileSync(filename, 'utf-8');
+	var file_content = fs.readFileSync(filename);
 }
 catch(err) {
     console.log('Something went wrong: ' + err);
@@ -12,59 +22,65 @@ catch(err) {
 }
 
 var cursor = 0;
-var decoded_data = bdecode(file_content;
+var decoded_data = bdecode(file_content);
+console.log(decoded_data);
 
-
-/*
-* Decode function
-*/
-
-function bdecode (data) {
-	var first_char = data.charAt(cursor)
-	cursor++;
-
-	switch(first_char) {
-		case 'i':
-			// Integers
-			var integer = '';
-			for (character = ''; character != 'e'; cursor++) {
-				integer += character;
-				character = data.charAt(cursor);
-			}
-			console.log(parseInt(integer));
-			break;
-
-		case 'l':
-			// List
-			while (data.charAt(cursor) != 'e') {
-				cursor = bdecode(data, cursor);
-			}
-			// Skip the e
-			cursor++;
-			break;
-
-		case 'd':
-			while (data.charAt(cursor) != 'e') {
-				cursor = bdecode(data, cursor);
-			}
-			// Skip the e
-			cursor++;
-			break;
-		default:
-			// Strings
-			var str = '';
-			var str_len = first_char;
-			while (data.charAt(cursor) != ':') {
-				str_len += data.charAt(cursor);
-				cursor++;
-			}
-			// Skip the colon
-			cursor++;
-			for (i = 0; i < str_len; i++) {
-				str += data.charAt(cursor);
-				cursor++;
-			}
-			console.log(str);
+/**
+ * Find the next ocurrence of value, starting onthe actual
+ * cursor position
+ */
+function findIndex(data, value) {
+	for (var i = cursor; i < data.length; i++){
+		if (data[i] === value) {
+			return i;
+		}
 	}
 }
 
+/**
+ * Data decoder
+ */
+function bdecode (data) {
+	var first_char = data[cursor];
+	cursor++;
+
+	switch(first_char) {
+		case i_CHAR:
+			// Integers
+			var end = findIndex(data, e_CHAR);
+			var value = data.slice(cursor, end);
+			cursor = end + 1;
+
+			return value;
+
+		case l_CHAR:
+			// List
+			var value = [];
+			while (data[cursor] != e_CHAR) {
+				value.push(bdecode(data));
+			}
+			// Skip the e
+			cursor++;
+
+			return value;
+
+		case d_CHAR:
+			var value = {};
+			while (data[cursor] != e_CHAR) {
+				value[bdecode(data)] = bdecode(data);
+			}
+			// Skip the e
+			cursor++;
+
+			return value;
+
+		default:
+			// Strings
+			var colon_position = findIndex(data, colon_CHAR);
+			var length_value = parseInt(data.slice(cursor - 1, colon_position).toString());
+			var value = data.slice(colon_position + 1, colon_position + length_value + 1);
+			cursor = colon_position + length_value + 1;
+
+			return value;
+	}
+}
