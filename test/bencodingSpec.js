74a2a82b7f7a76ebd916e44e96d91e232400c719
carletex <oceanrdn@gmail.com>
2014-07-29 10:07:08 -0400

    Write more tests. Refactor tests with beforeEach function. Change cursor name to _cursor

--- test/bencodingSpec.js
+++ test/bencodingSpec.js
@@ -2,14 +2,45 @@ var expect = require("chai").expect;
 var Bencoding = require("../lib/bencoding.js");
 
 describe("#decode()", function(){
+	var decoder;
+
+	beforeEach(function () {
+    	decoder = new Bencoding();
+  	});
+	it("should return an empty buffer when passing an empty string", function(){
+	   var result = decoder.decode('');
+	   expect(result).to.deep.eq(new Buffer(''));
+	});
+	it("should return an empty buffer when passing an empty buffer", function(){
+	   var result = decoder.decode('');
+	   expect(result).to.deep.eq(new Buffer(''));
+	});
 	it("should decode an integer", function(){
-	   var decoder = new Bencoding();
-	   var result = decoder.decode('i1e');
-	   expect(result).to.eq(1);
+	   var result = decoder.decode(new Buffer(''));
+	   expect(result).to.deep.eq(new Buffer(''));
 	});
-	it("should decode an string", function(){
-		var decoder = new Bencoding();
+	it("should decode a string", function(){
 		var result = decoder.decode('4:test');
-		expect(result.toString()).to.eq('test');
+		expect(result).to.deep.equal(new Buffer('test'));
 	});
+	it("should decode a list", function(){
+		var result = decoder.decode('l4:testi1ee');
+		expect(result).to.deep.equal([new Buffer('test'), 1]);
+	});
+	it("should decode a dictionary", function(){
+		var result = decoder.decode('d4:testi1ee');
+		expect(result).to.deep.equal({test: 1});
+	});
+	it("should decode a dictionary with nested elements", function(){
+		var result = decoder.decode('d4:testi1e5:test1d4:listli1ei2ei3eeee');
+		expect(result).to.deep.equal(
+		{
+        	"test": 1,
+        	"test1": {
+          		"list": [1,2,3]
+        	}
+       	});
+	});
+
+
 });
